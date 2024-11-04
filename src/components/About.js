74f6a048ebdd85553c7e6cb540a0c5b4e8f51f39
C-a-n-j <eddymacharia450@gmail.com>
2024-11-04 13:33:00 +0300

    jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati sint dignissimos explicabo delectus veniam reprehenderit in dolorum deserunt ipsa commodi repellendus dolores accusamus, minus natus earum aut distinctio nesciunt veritatis?</p>
+    <img src={image} alt="I made this"/>
+    </div>;
 }
 
 export default About;
